42de915cbe126440eaa5c644404ccd3ce2017d82
Efim Gutnik <bylochka96@gmail.com>
2026-02-03 15:51:36 +0300

    JS-TS/Task8: done

--- JS-TS/solutions/todo-service.ts
+++ JS-TS/solutions/todo-service.ts
@@ -1,18 +1,30 @@
-import { TodoApi } from './todo-api';
-import { Todo } from './types';
+import {TodoApi} from './todo-api';
+import {Todo, TodoStatus} from './types';
 
 export class TodoService {
-  constructor(private readonly api: TodoApi) { }
+  constructor(private readonly api: TodoApi) {
+  }
 
   async create(title: string, description = ''): Promise<Todo> {
-    throw new Error('create: not implemented');
+    return await this.api.add({title, description});
   }
 
   async toggleStatus(id: number): Promise<Todo> {
-    throw new Error('toggleStatus: not implemented');
+    const all: Todo[] = await this.api.getAll();
+    const todo = all.find(todo => {
+      return todo.id === id
+    })
+    return todo.status === TodoStatus.COMPLETED
+      ? await this.api.update(id, {status: TodoStatus.IN_PROGRESS})
+      : await this.api.update(id, {status: TodoStatus.COMPLETED});
+
   }
 
   async search(keyword: string): Promise<Todo[]> {
-    throw new Error('search: not implemented');
+    const arrayOfTodo = await this.api.getAll();
+    return arrayOfTodo.filter((todo) => {
+      return todo.title.toLowerCase().includes(keyword.toLowerCase())
+        || todo.description.toLowerCase().includes(keyword.toLowerCase());
+    });
   }
 }
